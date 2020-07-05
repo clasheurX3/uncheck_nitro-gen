63ba803c97ef5612895fe1d19024d7017f1fa569
ShotoYT <59451119+ShotoYT@users.noreply.github.com>
2020-07-05 14:21:48 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,101 @@
+import random
+import bs4
+import pyfiglet
+from colorama import Back, Fore, Style, deinit, init
+ascii_banner = pyfiglet.figlet_format("NITRO GEN")
+print(ascii_banner)
+
+print("Gen uncheck by Shoto ^^. (USE code LASKA sur Fortnite si tu y joues bg) ")
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+num = "1234567890"
+
+all = lower + upper + num
+length = 16
+endlink = "".join(random.sample(all,length))
+discord = "https://discord.gift/"
+link = discord + endlink
+print(link)
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+num = "1234567890"
+
+all = lower + upper + num
+length = 16
+endlink = "".join(random.sample(all,length))
+discord = "https://discord.gift/"
+link = discord + endlink
+print(link)
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+num = "1234567890"
+
+all = lower + upper + num
+length = 16
+endlink = "".join(random.sample(all,length))
+discord = "https://discord.gift/"
+link = discord + endlink
+print(link)
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+num = "1234567890"
+
+all = lower + upper + num
+length = 16
+endlink = "".join(random.sample(all,length))
+discord = "https://discord.gift/"
+link = discord + endlink
+print(link)
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+num = "1234567890"
+
+all = lower + upper + num
+length = 16
+endlink = "".join(random.sample(all,length))
+discord = "https://discord.gift/"
+link = discord + endlink
+print(link)
+
+all = lower + upper + num
+length = 16
+endlink = "".join(random.sample(all,length))
+discord = "https://discord.gift/"
+link = discord + endlink
+print(link)
+
+all = lower + upper + num
+length = 16
+endlink = "".join(random.sample(all,length))
+discord = "https://discord.gift/"
+link = discord + endlink
+print(link)
+
+all = lower + upper + num
+length = 16
+endlink = "".join(random.sample(all,length))
+discord = "https://discord.gift/"
+link = discord + endlink
+print(link)
+
+all = lower + upper + num
+length = 16
+endlink = "".join(random.sample(all,length))
+discord = "https://discord.gift/"
+link = discord + endlink
+print(link)
+
+all = lower + upper + num
+length = 16
+endlink = "".join(random.sample(all,length))
+discord = "https://discord.gift/"
+link = discord + endlink
+print(link)
+
+all = lower + upper + num
+length = 16
+endlink = "".join(random.sample(all,length))
+discord = "https://discord.gift/"
+link = discord + endlink
+print(link)
